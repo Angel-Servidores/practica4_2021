86b4cd7ba53ce5db14c46c3d301d98f36b568ef1
Angel-Servidores <L17181094@slp.tecnm.mx>
2021-11-16 12:52:56 -0600

    Mi nombre

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,6 +64,8 @@
         </style>
     </head>
     <body>
+        <h1 style="color:#ff8000">Angel Miguel S&aacute;nchez Bravo </h1><br/>
+        
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
